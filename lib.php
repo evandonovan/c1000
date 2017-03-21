cc2da00784e23322019851e8a64a8e5eb05131d0
Evan Donovan <outofegypt@gmail.com>
2017-03-20 23:16:06 -0400

    Added the reply function to lib

--- lib.php
+++ lib.php
@@ -142,3 +142,19 @@ function c1000_check_course_enrollment($courseid) {
   $enrolled = is_enrolled($context, $USER->id, '', TRUE);
   return $enrolled;
 }
+
+
+function c1000_build_reply_json($type = C1000_STATUS_PASS) {
+  $json_data = '';
+  
+  // Set JSON POST fields
+  $fields = array(array(
+    'SharedSecret' => array('Secret' => C1000_SECRET, 'Company' => C1000_COMPANY, 'status' => $type)));
+  if($type == C1000_STATUS_PASS) {
+    $fields[0]['URL']['URL'] = 'http://itwillbehere.com';
+  }
+  else {
+    $fields[0]['URL']['URL'] = '';
+  }
+  return $json_data;
+}

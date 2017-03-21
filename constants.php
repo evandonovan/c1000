d3bf84a6c25eb85c78b0a13476e421a5262e8e66
Evan Donovan <outofegypt@gmail.com>
2017-03-20 23:17:01 -0400

    updated statuses

--- constants.php
+++ constants.php
@@ -15,6 +15,6 @@ define('C1000_ERROR_NO_COURSES', 'local/c1000/error_no_courses.php');
 define('C1000_COURSE_FF', '74');
 define('C1000_COURSE_WL', '79');
 
-// types of replies
-define('C1000_REPLY_SUCCESS', TRUE);
-define('C1000_REPLY_FAILURE', FALSE);
+// reply statuses from curtis1000 API
+define('C1000_STATUS_PASS', 'Pass');
+define('C1000_STATUS_FAIL', 'Fail');
